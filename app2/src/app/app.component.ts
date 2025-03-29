bd4194c279a8ca92ff8f29b188b25ed5c33c0a33
yash <gyash986@gmail.com>
2025-03-29 21:00:46 +0530

    authguard and lazy loading ex

--- app2/src/app/app.component.ts
+++ app2/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReversePipe } from './reverse.pipe';
 import { Form1Component } from "./form1/form1.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Form1Component],
+  imports: [RouterOutlet, Form1Component,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app2';
   textSample='Hi this is test string';
+  loading!: false;
 }
--- app2/src/app/app.routes.ts
+++ app2/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { FeatureComponent } from './feature/feature.component';
+import { Form1Component } from './form1/form1.component';
+import { authGuardGuard } from './auth-guard.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+//   {
+// path:'feature', loadComponent: ()=> import('./feature/feature.component').then(m=>m.FeatureComponent)
+//   },import { authGuardGuard } from './auth-guard.guard';
+
+
+//   { path: '', redirectTo: '/feature', pathMatch: 'full' },
+
+
+{ path: 'home', component: FeatureComponent, canActivate: [authGuardGuard] },
+{ path: 'login', component: Form1Component },
+{ path: '', redirectTo: '/login', pathMatch: 'full' }
+
+];
